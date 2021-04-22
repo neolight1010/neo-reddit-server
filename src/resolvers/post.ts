fe08fed0d377153ff184b6c8d820c404c30f09c0
Anthony Suárez <neolight1010@gmail.com>
2021-04-21 19:34:05 -0500

    Add post (by id) query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from "../entities/Post";
 import { EntityManagerContext } from "../types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: EntityManagerContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: EntityManagerContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
