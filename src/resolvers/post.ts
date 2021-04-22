04839c154f36d45ebd4145f2ef0b6e078109d218
Anthony Suárez <neolight1010@gmail.com>
2021-04-21 19:53:06 -0500

    Add create and update post mutations

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { EntityManagerContext } from "../types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -15,5 +15,30 @@ export class PostResolver {
     @Ctx() { em }: EntityManagerContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: EntityManagerContext
+  ): Promise<Post> {
+    const post = new Post((title = title));
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", { nullable: false }) title: string,
+    @Ctx() { em }: EntityManagerContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+
+    if (!post) return null;
+    post.title = title;
+
+    await em.flush();
+    return post;
   }
 }
