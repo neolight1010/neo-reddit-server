fc3d515ac7998cda01221876678e374f86379d14
Anthony Suárez <neolight1010@gmail.com>
2021-05-18 13:50:37 -0500

    Add simple User entity.

---
+++ src/entities/User.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property({ unique: true })
+  username!: string;
+
+  @Property()
+  password!: string;
+
+  @Field()
+  @Property()
+  createdAt: Date = new Date();
+
+  @Field()
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  constructor(username: string) {
+    this.username = username;
+  }
+}
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __dbName__, __dbUser__, __dbPassword__, __debug__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: __dbName__,
   user: __dbUser__,
   password: __dbPassword__,
