045093b91778aa8077282347a2dee004a7b6fa03
Anthony Suárez <neolight1010@gmail.com>
2021-05-20 00:38:38 -0500

    Add User register validations.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -14,11 +14,23 @@ export class UserResolver {
     @Arg("registerInfo") registerInfo: UsernamePasswordInput,
     @Ctx() { em }: EntityManagerContext
   ): Promise<UserResponse> {
+    if (registerInfo.username.length <= 2) {
+      return { error: "Username must have at least 3 characters." };
+    }
+
+    if (registerInfo.password.length <= 4) {
+      return { error: "Password must have at least 5 characters." };
+    }
+
+    // Check if username is taken.
+    const taken = await em.findOne(User, { username: registerInfo.username });
+    if (taken) return { error: "Username is already taken." };
+
     const hashedPassword = await hash(registerInfo.password);
 
     const user = new User(registerInfo.username, hashedPassword);
-    await em.persistAndFlush(user);
 
+    await em.persistAndFlush(user);
     return { user };
   }
 
